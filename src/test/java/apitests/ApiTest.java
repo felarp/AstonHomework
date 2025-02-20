2cb3884f70dd0fdf765f1e7ea31d8a98267f907f
Igor Ogadzanyan <felarp@mail.ru>
2025-02-20 13:13:22 +0300

    17 finish

--- src/test/java/apitests/ApiTest.java
+++ src/test/java/apitests/ApiTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.Test;
 import java.util.HashMap;
 import java.util.Map;
 
-import static org.assertj.core.api.Assertions.assertThat;
 
 public class ApiTest extends BaseApiTest {
     @Test
