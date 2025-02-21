4b4f223592d5c506c506afe47975b176608e04c4
Igor Ogadzanyan <felarp@mail.ru>
2025-02-21 07:49:53 +0300

    18 first

--- src/test/java/tests/MtsOnlinePaymentTest.java
+++ src/test/java/tests/MtsOnlinePaymentTest.java
@@ -10,23 +10,20 @@ import pages.MtsServicePage;
 public class MtsOnlinePaymentTest extends BaseTest {
     @ParameterizedTest
     @ValueSource(strings = {"Услуги связи", "Домашний интернет", "Рассрочка", "Задолженность"})
-    @DisplayName("Проверка отображения полей и их надписей для услуги")
+    @DisplayName("Проверка отображения полей и их надписей для услуги: {arguments}")
     void shouldCheckFieldsForService(String serviceType) {
         paymentPage.checkFieldsForService(serviceType);
     }
-
 
     @ParameterizedTest
     @CsvSource({
             "(29)777-77-77, 10",
     })
-    @DisplayName("Проверка заполнения телефона, суммы и появления модального окна")
+    @DisplayName("Проверка заполнения телефона и суммы, а также появления модального окна для номера: {0} и суммы: {1}")
     public void testMtsServicePage(String phoneNumber, String amount) {
         new MtsServicePage(driver)
                 .fillFieldsAndSubmit(phoneNumber, amount)
                 .verifyPaymentDetails();
-
     }
 }
-
 
